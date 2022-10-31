ed215066ebc457f96e5aa82a7ad86da262bd9373
haf4computers@gmail.com <iberdahl@NOF.local>
2022-10-31 12:17:58 -0500

    Add a try and finally statement

--- SMTP.ps1
+++ SMTP.ps1
@@ -1,12 +1,13 @@
+Write-Output "Enter in the full domain name starting from '@'"
 $addy = Read-Host -Prompt "New Domain"
 $users = Get-EXOMailbox 
 
 foreach ($user in $users.Alias) {
-    $new = $user+'@'+$addy
-    Set-Mailbox -Identity "$user" -WindowsEmailAddress "$new" -MicrosoftOnlineServicesID "$new"
+    try {
+        $new = $user+'@'+$addy
+        Set-Mailbox -Identity "$user" -WindowsEmailAddress "$new" -MicrosoftOnlineServicesID "$new"
+    }
+    Finally {
     Get-EXOMailbox -Identity "$user" | Format-List UserPrincipalName, primarySMTPAddress, EmailAddresses
-}
-
-if ($args[0] -eq '-h','--help') {
-    Write-Output "Enter in the full domain name starting from '@'"
+    }
 }
